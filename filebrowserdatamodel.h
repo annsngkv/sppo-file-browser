5410f09e130c09a28c93d5be06ac69eb4baa8835
annsngkv <snegarkova@yandex.ru>
2021-06-09 04:40:34 +0700

    Add user-defined model to working with folder's data

--- filebrowserdatamodel.h
+++ filebrowserdatamodel.h
@@ -1,17 +1,37 @@
 #ifndef FILEBROWSERDATAMODEL_H
 #define FILEBROWSERDATAMODEL_H
-
-#include <QObject>
-
-class FileBrowserDataModel : public QObject
+#include <QAbstractTableModel>
+#include <QList>
+#include <QString>
+#include "somedata.h"
+class FileBrowserDataModel : public QAbstractTableModel
 {
     Q_OBJECT
-public:
-    explicit FileBrowserDataModel(QObject *parent = nullptr);
+    public:
+        FileBrowserDataModel(QObject *parent = nullptr, QList<SomeData> dt = QList<SomeData>());
+        //Минимальный и обязательный набор необходимых методов
+        //Так как нам требуется только отображать данные, то этого набора достаточно
 
-signals:
-
-public slots:
+        int rowCount(const QModelIndex &parent) const;
+        int columnCount(const QModelIndex &parent) const;
+        QVariant data(const QModelIndex &index, int role) const;
+        QVariant headerData(int section, Qt::Orientation orientation, int role) const;
+        //Функция для установки данных в модель
+        void setSomeData(const QList<SomeData> &dt) {dataModel = dt;}
+        //Функция для корректного обновления данных в таблице
+        void needReset() {
+            beginResetModel();
+            endResetModel();
+        }
+    private:
+        //Определим перечисление NameColumn для индексации столбцов нашей таблицы.
+        enum NameColumn {
+            NAME = 0,
+            SIZE,
+            PERCENT
+        };
+        //Представим данные модели в виде списка.
+        QList<SomeData> dataModel;
 };
 
-#endif // FILEBROWSERDATAMODEL_H
+#endif
